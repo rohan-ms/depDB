001be75062512764092a1d51c6c257e617129273
rohan-ms <007rohanms@gmail.com>
2022-03-02 23:23:22 +0530

    added delete route

--- controller/depController.js
+++ controller/depController.js
@@ -14,4 +14,17 @@ const changeDep=(req,res)=>{
     res.send("OK")
   }
 
-export {changeDep}
+const delDep=(req,res)=>{
+    const id=req.params.id 
+    let depData=JSON.parse(fs.readFileSync('./public/emp.json','utf-8'))
+    for(let i=0;i<depData.length;i++){
+        if(depData[i].id==parseInt(id)){
+            depData.splice(i,1)
+        }
+    }
+    fs.writeFileSync('./public/emp.json',JSON.stringify(depData))
+    res.send("OK")
+
+}
+
+export {changeDep,delDep}
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,6 @@ import express from "express"
 import * as depController from "./../controller/depController.js"
 const Router=express.Router()
 
-Router.route('/changeDep/:id').put(depController.changeDep)
+Router.route('/changeDep/:id').put(depController.changeDep).delete(depController.delDep)
 
 export default Router
