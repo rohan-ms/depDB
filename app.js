08bc172478731348a56f7031d8d08a7251b41fef
rohan-ms <007rohanms@gmail.com>
2022-10-17 18:25:36 +0530

    Implemented or fixed somthing

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-import express from "express"
-import Router from "./routes/routes.js"
-import morgan from "morgan"
+import express from "express";
+import Router from "./routes/routes.js";
+import morgan from "morgan";
 
 // import path from "path"
 // import { fileURLToPath } from "url"
-const app=express()
+const app = express();
 
-app.use(express.json())
-app.use(morgan('tiny'))
+app.use(express.json());
+app.use(morgan("tiny"));
 // console.log(path)
 // const filepath=fileURLToPath(import.meta.url)
 // const pathnew=path.join(filepath,'')
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/',Router)
+app.use("/", Router);
 
-app.listen(5000,()=>{
-  console.log("started on port 5000")
-})
+console.log("testing");
+
+app.listen(5000, () => {
+  console.log("started on port 5000");
+});
